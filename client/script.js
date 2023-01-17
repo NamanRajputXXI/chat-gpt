68e46243dede00b4d30efbd47dac8f2112f4d883
Naman Rajput <namanrajput801@gmail.com>
2023-01-17 20:39:23 +0530

    feat:work in script file

---
+++ client/script.js
@@ -0,0 +1,6 @@
+import bot from "./assets/bot.svg";
+import user from "./assets/user.svg";
+const form = document.querySelector("form");
+const chatContainer = document.querySelector("#chat_container");
+
+let loadInterval;
